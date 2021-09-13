ac7749abfca2727dc9ac9a536555fce7c838fb14
Medeiros92 <thalesbehringer@gmail.com>
2021-09-13 01:50:00 -0300

    Create Button Event in modal.js

--- modal.js
+++ modal.js
@@ -1,4 +1,11 @@
-//Criando divs
+
+//criando const bt
+const bt = document.querySelector('#openModal')
+
+//Criando evento do botão
+bt.addEventListener('click', function(){
+
+    //Criando divs
 const divWin = document.createElement('div');
 const divWinMsg = document.createElement('div');
 
@@ -12,10 +19,11 @@ body.append(divWin);
 divWin.append(divWinMsg);
 
 // Add classe na div Pattern
-divWin.setAttribute('class', 'modal-wrapper invisible fixed bg-gray-500 w-screen h-screen flex items-center justify-center');
+divWin.setAttribute('class', 'modal-wrapper fixed bg-gray-500 w-screen h-screen flex items-center justify-center');
 
 //Add classe na div Child
 divWinMsg.setAttribute('class', 'p-8 bg-white text-center rounded');
 
 // Add texto na div Child
-divWinMsg.innerText = "Tecle ESC para fechar"
+divWinMsg.innerText = "Tecle ESC para fechar";
+})
