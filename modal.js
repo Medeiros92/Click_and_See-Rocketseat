d34cf5130d1ef3353b10eeaf2999d7a938b77ac9
Medeiros92 <thalesbehringer@gmail.com>
2021-09-13 01:36:54 -0300

    ADD divs in modal.js

--- modal.js
+++ modal.js
@@ -5,3 +5,8 @@ const divWinMsg = document.createElement('div');
 // Criando const Body
 const body = document.querySelector('body');
 
+// Add div Pattern
+body.append(divWin);
+
+//Add div Child
+divWin.append(divWinMsg);
